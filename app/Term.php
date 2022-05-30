52f58f6ce129ea5591ec264fd8b3a6a1cffa08c7
thanhtinh <thanhtinh030291@gmail.com>
2022-05-30 11:25:47 +0700

    update

--- app/Term.php
+++ app/Term.php
@@ -26,7 +26,7 @@ class Term extends BaseModel
                 'group' => explode(".", $this->name)[0],
                 'num'   => explode(".", $this->name)[1],
                 'name' => $this->name,
-                'content' => '<p style="text-align: justify;"><span style="font-family: arial, helvetica, sans-serif;">Điều ' .$this->name ." ". preg_replace('/(<p>)/', "", $this->description.'</span>', 1)
+                'content' => '<p style="text-align: justify;font-size: 10pt;"><span style="font-size: 10pt;font-family: arial, helvetica, sans-serif;">Điều ' .$this->name ." ". preg_replace('/(<p>)/', "", $this->description.'</span>', 1)
             ];
         return $q;
     }
